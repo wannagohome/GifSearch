184ada038ef1c384e08efad92309cf1cac63ec1f
wannagohome <iwwuf7@gmail.com>
2022-05-02 19:10:06 +0900

    feat: make viewmodel

---
+++ GifSearch/Source/Presentation/Scenes/Search/SearchViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  SearchViewModel.swift
+//  GifSearch
+//
+//  Created by Jinho Jang on 2022/05/02.
+//
+
+import Foundation
+
+protocol SearchInput {
+    func typeSearchText(_ str: String)
+}
+
+protocol SearchOutput {
+    var errorMessage: ((String) -> ())? { get set }
+    var reloadTable: (([GIFModel]) -> ())? { get set}
+}
+
+protocol SearchViewModelType: SearchInput, SearchOutput {}
+
+final class SearchViewModel: SearchViewModelType {
+    
+    // MARK: - Properties
+    private let usecase: SearchUseCaseType
+    
+    // MARK: - Initialization
+    init(usecase: SearchUseCaseType) {
+        self.usecase = usecase
+    }
+    
+    // MARK: - States
+    private var models: [GIFModel] = []
+    
+    // MARK: - Outputs
+    var errorMessage: ((String) -> ())?
+    var reloadTable: (([GIFModel]) -> ())?
+    
+    // MARK: - Inputs
+    func typeSearchText(_ str: String) {
+        usecase.search(query: str) { [weak self] result in
+            switch result {
+            case .success(let models):
+                self?.models = models
+                self?.reloadTable?(models)
+            case .failure(let error):
+                self?.errorMessage?(error.localizedDescription)
+            }
+        }
+    }
+}
