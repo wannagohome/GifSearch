9e201af3a071aa094bd93276c5a862aa25c02637
wannagohome <iwwuf7@gmail.com>
2022-05-02 10:19:18 +0900

    first commit

---
+++ GifSearch/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GifSearch
+//
+//  Created by Jinho Jang on 2022/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
